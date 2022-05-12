ec0d48f05d5c10382ff72cf88738412f1fb8f8ca
chepkiruidorothy <dorokakz59@gmail.com>
2022-05-12 14:37:33 +0300

    commit message

--- bank/settings.py
+++ bank/settings.py
@@ -148,8 +148,7 @@ STATIC_URL = 'static/'
 STATIC_DIRS =[ os.path.join(BASE_DIR,'static'),]
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
