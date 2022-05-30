f4933b14baa70d3c97b3e7383c0ee89056a12944
chepkiruidorothy <dorokakz59@gmail.com>
2022-05-30 11:12:23 +0300

    gitignore

--- bank/settings.py
+++ bank/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 
